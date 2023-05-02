c046dbb46de10cf3a0e52443a6f58f927b50797c
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-02 22:09:56 +0800

    Navmenu

--- client/src/components/NavMenu.vue
+++ client/src/components/NavMenu.vue
@@ -9,9 +9,11 @@ const routes = reactive(router.getRoutes())
 <template>
   <div class="nav">
     <RouterLink
+      class="nav-item"
       v-for="route in routes"
       :key="route.name"
       :to="route.path"
+      active-class="active"
     >
       {{ route.name }}
     </RouterLink>
@@ -19,4 +21,20 @@ const routes = reactive(router.getRoutes())
 </template>
 
 <style scoped>
+.nav {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid var(--color-border);
+  width: 15%;
+}
+
+.nav-item {
+  padding: 1rem;
+  text-align: center;
+  border-bottom: 1px solid var(--color-border);
+}
+
+.nav .active {
+  background-color: var(--color-background-mute);
+}
 </style>
