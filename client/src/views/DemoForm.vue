e590bedb952a8f7372d5b8a55bf308ded7aa885a
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-03 13:21:30 +0800

    Update demo form to display v-model binding

--- client/src/views/DemoForm.vue
+++ client/src/views/DemoForm.vue
@@ -5,9 +5,15 @@ import TInput from 'coffeebrew_vue_components/src/components/form/TInput.vue'
 import TButton from 'coffeebrew_vue_components/src/components/form/TButton.vue'
 import TSelect from 'coffeebrew_vue_components/src/components/form/TSelect.vue'
 
-const country1 = ref('')
-const country2 = ref('')
-const country3 = ref('')
+const model = {
+  username: ref(''),
+  name: ref(''),
+  description: ref(''),
+  password: ref(''),
+  country1: ref(''),
+  country2: ref(''),
+  country3: ref('')
+}
 
 const countryOptions = ref([
   { value: 'sg', label: 'Singapore' },
@@ -23,24 +29,43 @@ function hello(text) {
 
 <template>
   <h1>Demo Form Elements</h1>
-  <TInput type="text" label="Username"/>
-  <TInput type="text" size="md" label="Name"/>
-  <TInput type="text" size="lg" label="Description"/>
-  <TInput type="password" label="Password" error-message="Min 8 characters"/>
+  <div class="form-container">
+    <div class="form">
+      <TInput v-model="model.username.value" type="text" label="Username"/>
+      <TInput v-model="model.name.value" type="text" size="md" label="Name"/>
+      <TInput v-model="model.description.value" type="text" size="lg" label="Description"/>
+      <TInput v-model="model.password.value" type="password" label="Password" error-message="Min 8 characters"/>
 
-  <TButton button-type="text" value="Very Long Button" icon="fa-sharp fa-solid fa-circle-down" @click="hello('Hello World!')"/>
-  <TButton button-type="text" size="lg" value="Very Long Long Button with Icon" icon="fa-sharp fa-solid fa-circle-down"/>
+      <TButton button-type="text" value="Very Long Button" icon="fa-sharp fa-solid fa-circle-down" @click="hello('Hello World!')"/>
+      <TButton button-type="text" size="lg" value="Very Long Long Button with Icon" icon="fa-sharp fa-solid fa-circle-down"/>
 
-  <TButton button-type="icon" icon="fa-sharp fa-solid fa-circle-down"/>
+      <TButton button-type="icon" icon="fa-sharp fa-solid fa-circle-down"/>
 
-  <TSelect v-model="country1" label="Country" name="country" id="country-1" :options="countryOptions"/>
-  <TSelect v-model="country2" size="sm" label="Country" name="country" id="country-2" :options="countryOptions"/>
-  <TSelect v-model="country3" size="lg" label="Country" name="country" id="country-3" :options="countryOptions" error-message="Min 8 characters"/>
+      <TSelect v-model="model.country1.value" label="Country" name="country" id="country-1" :options="countryOptions"/>
+      <TSelect v-model="model.country2.value" size="sm" label="Country" name="country" id="country-2" :options="countryOptions"/>
+      <TSelect v-model="model.country3.value" size="lg" label="Country" name="country" id="country-3" :options="countryOptions" error-message="Min 8 characters"/>
+    </div>
 
-  <div>Selected Country1: {{ country1 }}</div>
-  <div>Selected Country2: {{ country2 }}</div>
-  <div>Selected Country3: {{ country3 }}</div>
+    <div class="result">
+      <h2>Output</h2>
+      <div
+        v-for="key in Object.keys(model)"
+      >
+        {{ key }}: {{ model[key] }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.form-container {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  width: 100%;
+}
+
+.form {
+  width: 60%;
+}
 </style>
