61167d7601d37be85bead706672277dc85cc10c1
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-03 14:40:47 +0800

    Fix demo form v-model

--- client/src/views/DemoForm.vue
+++ client/src/views/DemoForm.vue
@@ -6,17 +6,17 @@ import TButton from 'coffeebrew_vue_components/src/components/form/TButton.vue'
 import TSelect from 'coffeebrew_vue_components/src/components/form/TSelect.vue'
 import TCheckbox from 'coffeebrew_vue_components/src/components/form/TCheckbox.vue'
 
-const model = {
-  username: ref(''),
-  name: ref(''),
-  description: ref(''),
-  password: ref(''),
-  country1: ref(''),
-  country2: ref(''),
-  country3: ref(''),
-  subscribe: ref(false),
-  agree: ref(false)
-}
+const model = ref({
+  username: '',
+  name: '',
+  description: '',
+  password: '',
+  country1: '',
+  country2: '',
+  country3: '',
+  subscribe: false,
+  agree: false
+})
 
 const countryOptions = ref([
   { value: 'sg', label: 'Singapore' },
@@ -34,22 +34,22 @@ function hello(text) {
   <h1>Demo Form Elements</h1>
   <div class="form-container">
     <div class="form">
-      <TInput v-model="model.username.value" type="text" label="Username"/>
-      <TInput v-model="model.name.value" type="text" size="md" label="Name"/>
-      <TInput v-model="model.description.value" type="text" size="lg" label="Description"/>
-      <TInput v-model="model.password.value" type="password" label="Password" error-message="Min 8 characters"/>
+      <TInput v-model="model.username" type="text" label="Username"/>
+      <TInput v-model="model.name" type="text" size="md" label="Name"/>
+      <TInput v-model="model.description" type="text" size="lg" label="Description"/>
+      <TInput v-model="model.password" type="password" label="Password" error-message="Min 8 characters"/>
 
       <TButton button-type="text" value="Very Long Button" icon="fa-sharp fa-solid fa-circle-down" @click="hello('Hello World!')"/>
       <TButton button-type="text" size="lg" value="Very Long Long Button with Icon" icon="fa-sharp fa-solid fa-circle-down"/>
 
       <TButton button-type="icon" icon="fa-sharp fa-solid fa-circle-down"/>
 
-      <TSelect v-model="model.country1.value" label="Country" name="country" id="country-1" :options="countryOptions"/>
-      <TSelect v-model="model.country2.value" size="sm" label="Country" name="country" id="country-2" :options="countryOptions"/>
-      <TSelect v-model="model.country3.value" size="lg" label="Country" name="country" id="country-3" :options="countryOptions" error-message="Min 8 characters"/>
+      <TSelect v-model="model.country1" label="Country" name="country" id="country-1" :options="countryOptions"/>
+      <TSelect v-model="model.country2" size="sm" label="Country" name="country" id="country-2" :options="countryOptions"/>
+      <TSelect v-model="model.country3" size="lg" label="Country" name="country" id="country-3" :options="countryOptions" error-message="Min 8 characters"/>
 
-      <TCheckbox v-model="model.subscribe.value" label="Subscribe newsletter"/>
-      <TCheckbox v-model="model.agree.value" label="Agree T&C" error-message="Required!"/>
+      <TCheckbox v-model="model.subscribe" label="Subscribe newsletter"/>
+      <TCheckbox v-model="model.agree" label="Agree T&C" error-message="Required!"/>
     </div>
 
     <div class="result">
