07465c204e943a953a58281f75187226a15d1759
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-04 19:25:22 +0800

    Demo TSelect

--- client/src/views/DemoForm.vue
+++ client/src/views/DemoForm.vue
@@ -1,6 +1,20 @@
 <script setup>
+import { ref } from 'vue'
+
 import TInput from 'coffeebrew_vue_components/src/components/form/TInput.vue'
 import TButton from 'coffeebrew_vue_components/src/components/form/TButton.vue'
+import TSelect from 'coffeebrew_vue_components/src/components/form/TSelect.vue'
+
+const country1 = ref('')
+const country2 = ref('')
+const country3 = ref('')
+
+const countryOptions = ref([
+  { value: 'sg', label: 'Singapore' },
+  { value: 'my', label: 'Malaysia' },
+  { value: 'th', label: 'Thailand' },
+  { value: 'vn', label: 'Vietnam' }
+])
 
 function hello(text) {
   alert(text)
@@ -18,6 +32,14 @@ function hello(text) {
   <TButton button-type="text" size="lg" value="Very Long Long Button with Icon" icon="fa-sharp fa-solid fa-circle-down"/>
 
   <TButton button-type="icon" icon="fa-sharp fa-solid fa-circle-down"/>
+
+  <TSelect v-model="country1" label="Country" name="country" id="country-1" :options="countryOptions"/>
+  <TSelect v-model="country2" size="sm" label="Country" name="country" id="country-2" :options="countryOptions"/>
+  <TSelect v-model="country3" size="lg" label="Country" name="country" id="country-3" :options="countryOptions" error-message="Min 8 characters"/>
+
+  <div>Selected Country1: {{ country1 }}</div>
+  <div>Selected Country2: {{ country2 }}</div>
+  <div>Selected Country3: {{ country3 }}</div>
 </template>
 
 <style scoped>
