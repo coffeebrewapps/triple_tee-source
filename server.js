31c90acc354135781900fb85739a42344672b965
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-04 21:33:38 +0800

    Update server to allow localhost origin

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
 
+const cors = require('cors');
+
 const common = require('./common');
 const users = require('./server/routes/users');
 
 const app = express();
 const port = process.env.PORT || common.DEFAULT_PORT;
 
+let corsConfig = {}
+
+if (process.env.NODE_ENV === 'development') {
+  corsConfig = {
+    origin: 'http://localhost'
+  }
+}
+
+app.use(cors(corsConfig));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
