d19ec7adcff2195ea8aa0c94c5ca14b06397e606
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-04 19:24:34 +0800

    Demo for TButton

--- client/src/views/DemoForm.vue
+++ client/src/views/DemoForm.vue
@@ -1,5 +1,10 @@
 <script setup>
 import TInput from 'coffeebrew_vue_components/src/components/form/TInput.vue'
+import TButton from 'coffeebrew_vue_components/src/components/form/TButton.vue'
+
+function hello(text) {
+  alert(text)
+}
 </script>
 
 <template>
@@ -8,6 +13,11 @@ import TInput from 'coffeebrew_vue_components/src/components/form/TInput.vue'
   <TInput type="text" size="md" label="Name"/>
   <TInput type="text" size="lg" label="Description"/>
   <TInput type="password" label="Password" error-message="Min 8 characters"/>
+
+  <TButton button-type="text" value="Very Long Button" icon="fa-sharp fa-solid fa-circle-down" @click="hello('Hello World!')"/>
+  <TButton button-type="text" size="lg" value="Very Long Long Button with Icon" icon="fa-sharp fa-solid fa-circle-down"/>
+
+  <TButton button-type="icon" icon="fa-sharp fa-solid fa-circle-down"/>
 </template>
 
 <style scoped>
