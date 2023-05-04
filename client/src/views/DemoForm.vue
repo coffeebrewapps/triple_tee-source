7dfad201cbbdc88e99c85b966da80c02c278cb28
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-04 19:23:56 +0800

    Create DemoForm, demo TInput

---
+++ client/src/views/DemoForm.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TInput from 'coffeebrew_vue_components/src/components/form/TInput.vue'
+</script>
+
+<template>
+  <h1>Demo Form Elements</h1>
+  <TInput type="text" label="Username"/>
+  <TInput type="text" size="md" label="Name"/>
+  <TInput type="text" size="lg" label="Description"/>
+  <TInput type="password" label="Password" error-message="Min 8 characters"/>
+</template>
+
+<style scoped>
+</style>
