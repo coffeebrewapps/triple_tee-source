071b93b1db13ab384c2038dbe56cacf824e37889
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-05 21:38:44 +0800

    Create shared routes

---
+++ server/routes/shared.js
@@ -0,0 +1,44 @@
+'use strict'
+
+exports.list = function(store) {
+  return function(req, res) {
+    const params = req.query;
+    console.log(`Requesting all data: ${JSON.stringify(params)}`);
+    res.send(store.list(params));
+  }
+};
+
+exports.view = function(store) {
+  return function(req, res) {
+    const params = req.params;
+    const id = parseInt(params.id);
+    console.log(`Viewing record[${id}]`);
+
+    res.send(store.view(id));
+  }
+};
+
+exports.update = function(store) {
+  return function(req, res) {
+    const params = req.body;
+    const id = parseInt(req.params.id);
+    delete params.id;
+
+    console.log(`Updating record[${id}]: ${JSON.stringify(params)}`)
+    res.send({
+      record: store.update(id, params)
+    });
+  }
+};
+
+exports.remove = function(store) {
+  return function(req, res) {
+    const params = req.params;
+    const id = parseInt(params.id);
+    console.log(`Removing record[${id}]`);
+
+    res.send({
+      success: store.remove(id)
+    });
+  }
+};
