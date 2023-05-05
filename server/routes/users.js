7f1941f6f0d380321fb73b7a940822196487ad76
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-05 21:38:57 +0800

    Update users routes to use shared

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,38 +1,9 @@
 'use strict'
 
+const shared = require('./shared');
 const usersStore = require('../stores/users');
 
-exports.list = function(req, res) {
-  const params = req.query;
-  console.log(`Requesting all users: ${JSON.stringify(params)}`);
-  res.send(usersStore.list(params));
-};
-
-exports.view = function(req, res) {
-  const params = req.params;
-  const id = parseInt(params.id);
-  console.log(`Viewing user: ${id}`);
-
-  res.send(usersStore.view(id));
-};
-
-exports.update = function(req, res) {
-  const params = req.body;
-  const id = parseInt(req.params.id);
-  delete params.id;
-
-  console.log(`Updating user[${id}]: ${JSON.stringify(params)}`)
-  res.send({
-    record: usersStore.update(id, params)
-  });
-};
-
-exports.remove = function(req, res) {
-  const params = req.params;
-  const id = parseInt(params.id);
-  console.log(`Removing user: ${id}`);
-
-  res.send({
-    success: usersStore.remove(id)
-  });
-};
+exports.list = shared.list(usersStore);
+exports.view = shared.view(usersStore);
+exports.update = shared.update(usersStore);
+exports.remove = shared.remove(usersStore);
