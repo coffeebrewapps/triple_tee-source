777566c9ba3a53a72ae781f9fd7c8ead13b8506d
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-16 16:09:12 +0800

    Dark mode toggle

--- client/src/components/TopNav.vue
+++ client/src/components/TopNav.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useAlertsStore } from '../stores/alerts'
 
 import {
@@ -8,19 +8,43 @@ import {
 
 const alerts = useAlertsStore()
 
+const theme = ref(true) // true: dark, false: light
+
+const themeIcon = computed(() => {
+  if (theme.value) {
+    return `fa-solid fa-moon`
+  } else {
+    return `fa-solid fa-sun`
+  }
+})
+
 function toggleAlerts() {
   alerts.toggle()
+}
+
+function toggleTheme() {
+  theme.value = !theme.value
+
+  document.body.classList.toggle('dark', theme.value)
+  document.body.classList.toggle('light', !theme.value)
 }
 </script>
 
 <template>
-  <div class="alert-toggle">
-    <TButton button-type="icon" icon="fa-solid fa-bell" @click="toggleAlerts"/>
+  <div class="toggles">
+    <div class="alert-toggle">
+      <TButton button-type="icon" icon="fa-solid fa-bell" @click="toggleAlerts"/>
+    </div>
+
+    <div class="theme-toggle">
+      <TButton button-type="icon" :icon="themeIcon" @click="toggleTheme"/>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.alert-toggle {
+.toggles {
   align-self: flex-end;
+  display: flex;
 }
 </style>
