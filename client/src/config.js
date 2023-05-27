36aa60cf6b0492b957ea58b630925781dc833da4
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-27 21:45:36 +0800

    Configurable data access for different build env

--- client/src/config.js
+++ client/src/config.js
@@ -3,18 +3,21 @@ const env = import.meta.env
 
 const useConfig = () => {
   const baseUrl = ref('')
-  const dataAccess = 'web'
+  const dataAccess = ref('')
 
   if (env.MODE === 'development') {
     const port = env.VITE_SERVER_PORT || env.VITE_DEFAULT_SERVER_PORT
+    dataAccess.value = env.VITE_DATA_ACCESS || env.VITE_DEFAULT_DATA_ACCESS
     baseUrl.value = `http://localhost:${port}`
+  } else if (env.MODE === 'staging') {
+    dataAccess.value = 'web'
   } else {
     baseUrl.value = ``
   }
 
   return {
     baseUrl: baseUrl.value,
-    dataAccess
+    dataAccess: dataAccess.value
   }
 }
 
