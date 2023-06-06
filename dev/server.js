0da6e57953e8cd7eec7a5d0fc384c9f2148a69f9
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-06-06 12:10:50 +0800

    Move dev app config file

--- dev/server.js
+++ dev/server.js
@@ -3,6 +3,6 @@ const { startServer } = require('../server/index.js');
 const port = process.env.PORT;
 
 const appRootDir = path.join(__dirname, '../');
-const appConfigPath = path.join(__dirname, '../app_config.json');
+const appConfigPath = path.join(__dirname, './app_config.json');
 
 startServer({ port, appConfigPath, appRootDir, logsRootDir: appRootDir });
