390ae6521ce53e1a75a09b9d9325b2c673d2505b
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-06-09 12:05:40 +0800

    Client-side reverse transaction

--- client/src/plugins/transactions/index.js
+++ client/src/plugins/transactions/index.js
@@ -1,11 +1,18 @@
+import { useStore } from './store';
+
 const route = {
   path: '/transactions',
   name: 'Transactions',
   component: () => import('./TransactionsPage.vue'),
 };
 
-const usePlugin = (router) => {
+const usePlugin = (router, dataStore) => {
+  const store = useStore(dataStore);
+
   router.addRoute(route);
+
+  dataStore.registerFunction('transactions', 'update', 'reverse', store.reverseTransaction);
+
   return route;
 };
 
---
+++ client/src/plugins/transactions/store.js
@@ -0,0 +1,81 @@
+export function useStore(dataStore) {
+  function reverseTransaction(modelClass, id, params) {
+    const result = dataStore.view('transactions', id, {});
+
+    if (!result.success) { return result; }
+
+    const record = result.record;
+
+    if (record.type !== 'income' && record.type !== 'expense') {
+      return {
+        success: false,
+        errors: {
+          type: ['notReversible'],
+        },
+      };
+    }
+
+    const reversalType = record.type === 'income' ? 'incomeReversal' : 'expenseReversal';
+    const reversalParams = {
+      type: reversalType,
+      transactionDate: new Date(),
+      description: `Reverse ${record.description}`,
+      amount: record.amount,
+      homeCurrencyAmount: record.homeCurrencyAmount,
+      currencyId: record.currencyId,
+    };
+
+    const { success, results } = dataStore.atomic(
+      [
+        {
+          id: 'createReversalTransaction',
+          params: reversalParams,
+          invoke: (reversalParams) => {
+            return dataStore.create('transactions', reversalParams);
+          },
+          rollback: (result) => {
+            dataStore.remove('transactions', result.record.id);
+          },
+        },
+        {
+          id: 'updateOriginalTransactionAssociation',
+          params: {
+            originalId: record.id,
+          },
+          invoke: ({ originalId }, pastResults) => {
+            const reversalTransactionId = pastResults[0].result.record.id;
+            return dataStore.update('transactions', originalId, { associatedTransactionId: reversalTransactionId });
+          },
+          rollback: (result) => {
+            dataStore.update('transactions', result.record.id, { associatedTransactionId: null });
+          },
+        },
+      ]
+    );
+
+    if (success) {
+      const originalTransaction = results[1].result.record;
+      const reversalTransaction = results[0].result.record;
+      const includes = {
+        associatedTransactionId: {
+          [reversalTransaction.id]: reversalTransaction,
+        },
+      };
+
+      return {
+        success: true,
+        record: Object.assign(
+          {},
+          originalTransaction,
+          { includes }
+        ),
+      };
+    } else {
+      return results[0].result;
+    }
+  }
+
+  return {
+    reverseTransaction,
+  }
+}
